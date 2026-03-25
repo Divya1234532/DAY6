9b88ddb0fb5add48eb61a6ed3acbe9e43bd13a81
Divya1234532 <dd1320983@gmail.com>
2026-03-25 11:08:06 +0530

    Create LC1470ShuffleTheArray.java

---
+++ LC1470ShuffleTheArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int i=0,j=n,k=0;
+        int[] arr=new int[2*n];
+        while(i<n&&j<2*n)
+        {
+            arr[k++]=nums[i++];
+            arr[k++]=nums[j++];
+        }
+        return arr;
+        
+    }
+}
